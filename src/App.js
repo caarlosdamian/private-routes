10bed3816635b9e5430cc5a62d38f00f726a7f87
caarlosdamian <dapecarlos@gmail.com>
2021-08-23 14:09:58 -0500

    Feature: Add private route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import ProtectedRoutes from "./pages/ProtectedRoutes";
+import Profile from "./pages/Profile";
+import { useState } from "react";
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
   return (
-    <Router>
-      <div className="App">Hola</div>
-    </Router>
+    <div className="app">
+      <Router>
+        <Route path="/" exact>
+          <Link to="/profile">
+            <button onClick={() => setIsAuth(true)}>Login</button>
+          </Link>
+          <button onClick={() => setIsAuth(false)}>LogOut</button>
+          <Link to="/profile">Go to Profile</Link>
+        </Route>
+        <ProtectedRoutes path="/profile" component={Profile} isAuth={isAuth} />
+      </Router>
+    </div>
   );
 }
 
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {withRouter} from 'react-router-dom'
+function Profile() {
+    return (
+        <div>
+            if you see this it means you are authenticated
+        </div>
+    )
+}
+
+export default withRouter(Profile)
