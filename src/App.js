b7775e1c81a6f58350215d8ae06a7d84a15a941d
caarlosdamian <dapecarlos@gmail.com>
2021-08-23 11:30:46 -0500

    Dependencies: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-  Hola
-    </div>
+    <Router>
+      <div className="App">Hola</div>
+    </Router>
   );
 }
 
